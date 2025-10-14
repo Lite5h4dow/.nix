8c339c0c03175a9763e152fffe220e4e0c44f973
Lamar Daughma <lite5h4dow+dev@gmail.com>
2025-10-14 18:15:47 +0000

    adding initial empty flake parts

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake{inherit inputs;}(
+      top@{config, withSystem, moduleWithSystem, ...}:{
+    
+      }
+    );
+}
