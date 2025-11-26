efb85cec819eb4ebe6aa9e87272cb33ae939c72c
litelotus <lite5h4dow+dev@gmail.com>
2025-11-26 21:52:58 +0000

    updating

--- flake.nix
+++ flake.nix
@@ -12,15 +12,25 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
     };
     nix-on-droid = {
       url = "github:nix-community/nix-on-droid";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
     };
     hyprshell.url = "github:H3rmt/hyprshell";
-    stylix.url = "github:nix-community/stylix/release-25.05";
+    stylix = {
+      url = "github:nix-community/stylix/release-25.05";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        # home-manager.follows = "home-manager";
+      };
+    };
     import-tree.url = "github:vic/import-tree";
   };
 
