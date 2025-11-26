85ac708612d6e7ce64f94bec2a4d4d0ebdc602d5
litelotus <lite5h4dow+dev@gmail.com>
2025-11-26 18:43:19 +0000

    updating map

--- modules/hosts/phone.nix
+++ modules/hosts/phone.nix
@@ -5,15 +5,11 @@
   ...
 }:
 {
-  flake.nixOnDroidConfigurations =
-    config.flake.modules.nixOnDroid or { }
-    |> lib.mapAttrs' (
-      name: module: {
-        inherit name;
-        value = inputs.nix-on-droid.lib.nixOnDroidConfiguration {
-          pkgs = import inputs.nixpkgs { system = "aarch64-linux"; };
-          modules = [ module ];
-        };
-      }
-    );
+  flake.nixOnDroidConfigurations = lib.mapAttrs' (name: module: {
+    inherit name;
+    value = inputs.nix-on-droid.lib.nixOnDroidConfiguration {
+      pkgs = import inputs.nixpkgs { system = "aarch64-linux"; };
+      modules = [ module ];
+    };
+  }) config.flake.modules.nixOnDroid or { };
 }
