934ff4819276943b542515c55dafc7bd489f1695
litelotus <lite5h4dow+dev@gmail.com>
2025-11-26 20:34:31 +0000

    updating

--- modules/style/colour-scheme.nix
+++ modules/style/colour-scheme.nix
@@ -1,10 +1,13 @@
-{ inputs, lib, ...}:
+{ inputs, lib, ... }:
 let
-  polyModule.stylix = {pkgs,...}:
-  {
-    base16scheme = pkgs.base16-schemes + /share/themes/catppuccin-macchiato.yaml;
-    polarity = "dark";
-  };
+  polyModule =
+    { pkgs, ... }:
+    {
+      stylix = {
+        base16scheme = pkgs.base16-schemes + /share/themes/catppuccin-macchiato.yaml;
+        polarity = "dark";
+      };
+    };
 in
 {
   flake.modules = {
