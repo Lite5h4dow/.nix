07f41e42d4cf1a3c81e298223386a26102772684
litelotus <lite5h4dow+dev@gmail.com>
2025-11-26 20:24:48 +0000

    updating

--- modules/style/fonts.nix
+++ modules/style/fonts.nix
@@ -1,38 +1,41 @@
-{ lib, ...}:
+{ lib, ... }:
 let
-  polyModule = polyArgs@{pkgs, ...}:{
-    stylix.fonts = {
+  polyModule =
+    polyArgs@{ pkgs, ... }:
+    {
+      stylix.fonts = {
 
-      sansSerif = lib.mkDefault {
-        package = pkgs.nerd-fonts.fira-code;
-        name = "FiraCode Nerd Font";
+        sansSerif = lib.mkDefault {
+          package = pkgs.nerd-fonts.fira-code;
+          name = "FiraCode Nerd Font";
+        };
+
+        serif = polyArgs.config.stylix.fonts.sansSerif;
+
+        monospace = {
+          package = pkgs.nerd-fonts.fira-mono;
+          name = "FiraCode Nerd Font Mono";
+        };
+
+        emoji = {
+          package = pkgs.google-fonts;
+          name = "Noto Color Emoji";
+        };
+
+        sizes = {
+          applications = 12;
+          desktop = 12;
+          popups = 12;
+          terminal = 12;
+        };
       };
 
-      serif = polyArgs.config.stylix.fonts.sansSerif;
-
-      monospace = {
-        package = pkgs.nerd-fonts.fira-mono;
-        name = "FiraCode Nerd Font Mono";
-      };
-
-      emoji = {
-        package = pkgs.google-fonts;
-        name = "Noto Color Emoji";
-      };
-
-      sizes = {
-        applications = 12;
-        desktop = 12;
-        popups = 12;
-        terminal = 12;
-      };
+      fonts.fontconfig.enable = true;
     };
-
-    fonts.fontconfig.enable = true;
-  };
 in
 {
-  
-  nixos.base = polyModule;
-  nixOnDroid.base = polyModule;
+  flake.modules = {
+    nixos.base = polyModule;
+    nixOnDroid.base = polyModule;
+  };
 }
