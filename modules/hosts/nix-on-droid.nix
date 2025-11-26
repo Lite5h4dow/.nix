8dc416dcdc88c6abda2565c4723b91c1fe56b047
litelotus <lite5h4dow+dev@gmail.com>
2025-11-26 22:35:38 +0000

    trying

--- modules/hosts/nix-on-droid.nix
+++ modules/hosts/nix-on-droid.nix
@@ -7,7 +7,7 @@
 {
   flake.nixOnDroidConfigurations = {
     "base" = inputs.nix-on-droid.lib.nixOnDroidConfiguration {
-      pkgs = import inputs.nixpkgs { system = "aarch64-linux"; };
+      # pkgs = import inputs.nixpkgs { system = "aarch64-linux"; };
       modules = [
         config.flake.modules.nixOnDroid.base
         {system.stateVersion = "24.05"; }
