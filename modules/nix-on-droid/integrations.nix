360ab4cb4758e0c42594e0d86bebfc4a8f01793a
litelotus <lite5h4dow+dev@gmail.com>
2025-11-26 22:20:56 +0000

    add android integrations

---
+++ modules/nix-on-droid/integrations.nix
@@ -0,0 +1,15 @@
+{
+  flake.modules.nixOnDroid.base = {
+    android-integration = {
+      am.enable = true;
+      termux-open.enable = true;
+      termux-open-url.enable = true;
+      termux-reload-settings.enable = true;
+      termux-setup-storage.enable = true;
+      termux-wake-lock.enable = true;
+      termux-wake-unlock.enable = true;
+
+      xdg-open.enable = true;
+    };
+  };
+}
