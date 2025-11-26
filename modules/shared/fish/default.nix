e6b6478c4b10f8728c50ea23508229a32ae15ca3
litelotus <lite5h4dow+dev@gmail.com>
2025-11-26 21:03:46 +0000

    updating

--- modules/shared/fish/default.nix
+++ modules/shared/fish/default.nix
@@ -2,4 +2,9 @@
   flake.modules.homeManager.base.programs.fish = {
     enable = true;
   };
+  flake.modules.nixOnDroid.base.environment = {pkgs,...}:{
+    packages = with pkgs;[
+      ncurses
+    ];
+  };
 }
