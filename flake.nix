e0e210d5be0d73bff060bd8302b1bb37aea4f5f5
litelotus <lite5h4dow+dev@gmail.com>
2025-11-26 19:51:34 +0000

    updating

--- flake.nix
+++ flake.nix
@@ -1,6 +1,12 @@
 {
   description = "Description for the project";
 
+  nixConfig = {
+    abort-on-warn = false;
+    extra-experimental-features = [ "pipe-operators" ];
+    allow-import-from-derivation = false;
+  };
+
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
@@ -18,8 +24,10 @@
     import-tree.url = "github:vic/import-tree";
   };
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-    imports = [(inputs.import-tree ./modules)];
-    _module.args.rootPath = ./. ;
-  };
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ (inputs.import-tree ./modules) ];
+      _module.args.rootPath = ./.;
+    };
 }
