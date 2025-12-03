c1050c9fa100f1338bec38c3eaedc16d5225afec
litelotus <lite5h4dow+dev@gmail.com>
2025-12-03 14:27:28 +0000

    adding

--- modules/shared/virtualisation/podman.nix
+++ modules/shared/virtualisation/podman.nix
@@ -1,26 +1,29 @@
 { ... }:
 let
   module' = {
-    virtualisation.podman = {
-      enable = true;
+    virtualisation = {
+      podman = {
+        enable = true;
+      };
+      oci-containers.backend = "podman";
     };
-    oci-containers.backend = "podman";
   };
 in
 {
   flake.modules = {
     nixos.base = module';
-    nixos.wsl = {pkgs,...}:
-    let
-      podman' =  pkgs.podman.overrideAttrs (oldAttrs:{
-        buildInputs = oldAttrs.buildInputs or [] ++ [pkgs.makeWrapper];
-        postInstall = oldAttrs.postInstall or "" + ''
-          wrapProgram $out/bin/podman --add-flags "-r"
-        '';
-      });
-    in
-    {
-      virtualisation.podman.package = podman';
-    };
+    nixos.wsl =
+      { pkgs, ... }:
+      let
+        podman' = pkgs.podman.overrideAttrs (oldAttrs: {
+          buildInputs = oldAttrs.buildInputs or [ ] ++ [ pkgs.makeWrapper ];
+          postInstall = oldAttrs.postInstall or "" + ''
+            wrapProgram $out/bin/podman --add-flags "-r"
+          '';
+        });
+      in
+      {
+        virtualisation.podman.package = podman';
+      };
   };
 }
