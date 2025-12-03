aa4f4ba701fec15ffdf280d4bf186307b4fc8fc2
litelotus <lite5h4dow+dev@gmail.com>
2025-12-03 14:46:49 +0000

    adding git status

--- modules/home-manager/shared/git/default.nix
+++ modules/home-manager/shared/git/default.nix
@@ -1,37 +1,37 @@
 {
   flake.modules.homeManager.base.programs.git = {
     enable = true;
-    # settings = {
-    #   user = {
-    #     name = "lamar daughma";
-    #     email = "lite5h4dow+dev@gmail.com";
-    #   };
+    settings = {
+      user = {
+        name = "lamar daughma";
+        email = "lite5h4dow+dev@gmail.com";
+      };
 
-    #   credential = {
-    #     # helper = "${pkgs.gitFull}/bin/git-credential-libsecret";
-    #     credentialStore = "secretservice";
-    #     "https://git.litelot.us".username = "litelotus";
-    #     "https://www.github.com".username = "lite5h4dow+dev@gmail.com";
-    #     "https://github.com".username = "lite5h4dow+dev@gmail.com";
-    #   };
+      credential = {
+        # helper = "${pkgs.gitFull}/bin/git-credential-libsecret";
+        # credentialStore = "secretservice";
+        "https://git.litelot.us".username = "litelotus";
+        "https://www.github.com".username = "lite5h4dow+dev@gmail.com";
+        "https://github.com".username = "lite5h4dow+dev@gmail.com";
+      };
 
-    #   init = {
-    #     defaultBranch = "main";
-    #   };
+      init = {
+        defaultBranch = "main";
+      };
 
-    #   push = {
-    #     autoSetupRemote = true;
-    #   };
+      push = {
+        autoSetupRemote = true;
+      };
 
-    #   url = {
-    #     # github.com
-    #     "ssh://git@github.com/".insteadOf = "github:";
-    #     "https://github.com/".insteadOf = "github+https:";
+      url = {
+        # github.com
+        "ssh://git@github.com/".insteadOf = "github:";
+        "https://github.com/".insteadOf = "github+https:";
 
-    #     # git.litelot.us
-    #     "ssh://git@git.litelot.us/".insteadOf = "git:";
-    #     "https://git.litelot.us/".insteadOf = "git+https:";
-    #   };
-    # };
+        # git.litelot.us
+        "ssh://git@git.litelot.us/".insteadOf = "git:";
+        "https://git.litelot.us/".insteadOf = "git+https:";
+      };
+    };
   };
 }
