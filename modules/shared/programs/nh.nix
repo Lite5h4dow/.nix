b54291100f8e6c71b163d0e23cea4a168d355c80
litelotus <lite5h4dow+dev@gmail.com>
2025-12-03 16:19:40 +0000

    updating

--- modules/shared/programs/nh.nix
+++ modules/shared/programs/nh.nix
@@ -7,7 +7,7 @@ let
         dates = "weekly";
         extraArgs = "--keep-since 14d --keep 5";
       };
-      flake = "git+https://github.com/Lite5h4dow/.nix";
+      flake = "git+https://github.com/Lite5h4dow/.nix.git";
     };
   };
 in
