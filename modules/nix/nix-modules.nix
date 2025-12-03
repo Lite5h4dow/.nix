9b47079ef4c8489d995e3e3159fe9dc301d326ac
litelotus <lite5h4dow+dev@gmail.com>
2025-12-03 14:06:22 +0000

    updating user

--- modules/nix/nix-modules.nix
+++ modules/nix/nix-modules.nix
@@ -1,9 +1,9 @@
 {inputs, lib, ...}:
 {
-  flake.modules.nixos.wsl = {
-    nixpkgs.config.permittedInsecurePackages = [
-      "openssl-1.1.1w"
-      "dotnet-sdk-6.0.428"
-    ];
-  };
+  # flake.modules.nixos.wsl = {
+  #   nixpkgs.config.permittedInsecurePackages = [
+  #     "openssl-1.1.1w"
+  #     "dotnet-sdk-6.0.428"
+  #   ];
+  # };
 }
--- modules/user/default.nix
+++ modules/user/default.nix
@@ -1,6 +1,12 @@
-{ ... }:
+{ config, ... }:
 {
   flake.meta.user = {
     name = "lotus";
+  };
+
+  flake.modules.nixos.base = {
+    users.users.${config.flake.meta.user.name} = {
+      isNormalUser = true;
+    };
   };
 }
