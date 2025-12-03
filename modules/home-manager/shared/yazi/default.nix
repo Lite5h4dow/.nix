7e6cc8ad008ad0f1d8d4c0a2114b0609c3d5e052
litelotus <lite5h4dow+dev@gmail.com>
2025-12-03 14:58:37 +0000

    updating

--- modules/home-manager/shared/yazi/default.nix
+++ modules/home-manager/shared/yazi/default.nix
@@ -1,18 +1,21 @@
 { ... }:
 {
-  flake.modules.homeManager.base.programs.yazi = {pkgs}:{
-    enable = true;
-    plugins = {
-      inherit (pkgs.yaziPlugins)
-        git
-        ouch
-        sudo
-        rsync
-        chmod
-        starship
-        wl-clipboard
-        yatline-catppuccin
-        ;
+  flake.modules.homeManager.base = { pkgs }:
+  {
+      programs.yazi = {
+        enable = true;
+        plugins = {
+          inherit (pkgs.yaziPlugins)
+            git
+            ouch
+            sudo
+            rsync
+            chmod
+            starship
+            wl-clipboard
+            yatline-catppuccin
+            ;
+        };
+      };
     };
-  };
 }
