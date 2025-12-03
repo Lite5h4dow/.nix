c0682c2f60ebaca64618fb96db89f6ac971333f9
litelotus <lite5h4dow+dev@gmail.com>
2025-12-03 23:29:31 +0000

    updating substitutors

--- flake.nix
+++ flake.nix
@@ -2,6 +2,23 @@
   description = "Description for the project";
 
   nixConfig = {
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+    ];
+    extra-substituters = [
+      "https://hyprland.cachix.org"
+    ];
+    extra-trusted-substituters = [
+      "https://hyprland.cachix.org"
+    ];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+    extra-trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
     warn-dirty = false;
     abort-on-warn = false;
     extra-experimental-features = [ "pipe-operators" ];
