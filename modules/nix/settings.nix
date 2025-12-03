175bee41fb20b8fd8ef647ab70f95a0d95abe24b
litelotus <lite5h4dow+dev@gmail.com>
2025-12-03 23:41:30 +0000

    updating

--- modules/nix/settings.nix
+++ modules/nix/settings.nix
@@ -18,24 +18,6 @@
         "root"
         "@wheel"
       ];
-
-      substituters = [
-        "https://cache.nixos.org/"
-        "https://nix-community.cachix.org"
-      ];
-      extra-substituters = [
-        "https://hyprland.cachix.org"
-      ];
-      extra-trusted-substituters = [
-        "https://hyprland.cachix.org"
-      ];
-      trusted-public-keys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-      extra-trusted-public-keys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      ];
     };
 
     flake.modules = {
