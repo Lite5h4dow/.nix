f66553046321f40db219c82dd0be7f9603ff6721
litelotus <lite5h4dow+dev@gmail.com>
2025-12-04 20:10:26 +0000

    updating

--- modules/shared/hardware/graphics.nix
+++ modules/shared/hardware/graphics.nix
@@ -1,8 +1,14 @@
 { ... }:
 let
-  module' = { pkgs, ...}:{
-    hardware.graphics.extraPackages = with pkgs;[ intel-media-driver intel-ocl intel-vaapi-driver ];
-  };
+  module' =
+    { pkgs, ... }:
+    {
+      hardware.graphics.extraPackages = with pkgs; [
+        intel-media-driver
+        intel-ocl
+        intel-vaapi-driver
+      ];
+    };
 in
 {
   flake.modules = {
