a748f88ed0d5fd1dc51013836d7e9fbe104a8076
lamar daughma <lite5h4dow+dev@gmail.com>
2025-12-05 10:04:53 +0000

    adding

--- modules/stylix/opacity.nix
+++ modules/stylix/opacity.nix
@@ -2,9 +2,6 @@
 let
   polyModule = {
     stylix.opacity = lib.genAttrs [ "applications" "desktop" "popups" "terminal" ] (n: 0.85);
-    stylix.targets.helix = {
-      transparent = true;
-    };
   };
 in
 {
