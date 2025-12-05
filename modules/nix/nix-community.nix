a3d106df9035967327d536c0bbcc5d2203fc8e99
litelotus <lite5h4dow+dev@gmail.com>
2025-12-05 22:57:06 +0000

    adding caches!

---
+++ modules/nix/nix-community.nix
@@ -0,0 +1,15 @@
+{ ... }:
+let
+  module' = {
+    nix.settings = {
+      extra-substituters = ["https://nix-community.cachix.org/"];
+      extra-trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+    };
+  };
+in
+{
+  flake.modules = {
+    nixos.base = module';
+    nixOnDroid.base = module';
+  };
+}
--- modules/shared/programs/gaming.nix
+++ modules/shared/programs/gaming.nix
@@ -11,5 +11,10 @@
         protontricks.enable = true;
       };
     };
+    nix.settings = {
+      allowInsecure = true;
+      extra-substituters = ["https://nix-gaming.cachix.org"];
+      extra-trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
+    };
   };
 }
