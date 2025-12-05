1e3ec57325bcde0928c8e397c455bd51023fcdfb
litelotus <lite5h4dow+dev@gmail.com>
2025-12-05 21:26:46 +0000

    adding abbreviations

---
+++ modules/home-manager/shared/fish/abbreviations.nix
@@ -0,0 +1,75 @@
+{ ... }:
+{
+  flake.modules.homeManager.base = { ... }:
+  let
+    cursor = expansion: {
+      setCursor = true;
+      inherit expansion;
+    };
+  in
+  {
+    programs.fish.shellAbbrs = {
+      hash = "sha256sum";
+      copy = "wl-copy";
+      paste = "wl-paste";
+      mp = "mkdir -p";
+      page = "$PAGER";
+      open = "xdg-open";
+      size = "du -sh";
+      "-" = "cd -";
+      cht = "cht.sh";
+
+      sc = "systemctl";
+      jc = "journalctl";
+      scu = "systemctl --user";
+      jcu = "journalctl --user";
+
+
+      # nix
+      n = "nix";
+      ne = "cd ~/.nixos";
+      nr = "nh os switch";
+      clean = "nh clean all --keep 5";
+      shell = "nix shell";
+      dev = "nix develop --command fish";
+      run = "nix run";
+      build = "nix build";
+      flake = "nix flake";
+      nfu = "nix flake update";
+      repl = "nix repl --expr 'import <nixpkgs> {}'";
+      bloat = "nix path-info -Sh /run/current-system";
+      unfree = cursor "NIXPKGS_ALLOW_UNFREE=1 % --impure";
+      insecure = cursor "NIXPKGS_ALLOW_INSECURE=1 % --impure";
+      broken = cursor "NIXPKGS_ALLOW_BROKEN=1 % --impure";
+
+      #pulumi
+      p = "pulumi";
+      pu = "pulumi up";
+      pd = "pulumi dn";
+      pn = "pulumi new";
+
+      # git
+      g = "git";
+      gc = "git commit";
+      gp = "git push";
+      gl = "git pull";
+      gst = "git status";
+      grhh = "git reset --hard";
+      gb = "git branch";
+      gm = "git merge";
+      gfa = "git fetch --all";
+      gpf = "git push --force";
+      gco = "git checkout";
+      gd = "git diff";
+      gs = "git switch";
+      gsc = "git switch --create";
+      ga = "git add";
+      gf = "git fetch";
+      gu = "git commit && git push";
+
+      # configs
+      ags-config = "cd ~/Projects/ags-config";
+
+    };
+  };
+}
