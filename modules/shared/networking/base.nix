6da5ac5f85c25a9a96a63dabb2e36a9c7a2ecc78
lamar daughma <lite5h4dow+dev@gmail.com>
2025-12-06 14:24:36 +0000

    adding

--- modules/shared/networking/base.nix
+++ modules/shared/networking/base.nix
@@ -4,11 +4,9 @@ let
     networking = {
       enableIPv6 = true;
       resolvconf.enable = false;
-      dhcpcd.enable = true;
       networkmanager = {
         enable = true;
         dns = "systemd-resolved";
-        dhcp = "dhcpcd";
       };
     };
 
