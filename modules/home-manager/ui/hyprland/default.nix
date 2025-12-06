cc74de9722072281761ed1a9d8825647d5cee985
lamar daughma <lite5h4dow+dev@gmail.com>
2025-12-06 00:10:50 +0000

    adding

--- modules/home-manager/ui/hyprland/default.nix
+++ modules/home-manager/ui/hyprland/default.nix
@@ -54,7 +54,9 @@
 
           group.groupbar = {
             gradients = true;
-            gradient_rounding = 10;
+            rounding = 10;
+            height = 20;
+            round_only_edges = false;
             indicator_height = 0;
             font_size = 12;
             gaps_in = 5;
