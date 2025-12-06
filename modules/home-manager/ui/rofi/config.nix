c4c5234adfae5f6d1f83e258115231fa9a473235
lamar daughma <lite5h4dow+dev@gmail.com>
2025-12-06 14:38:05 +0000

    updating

---
+++ modules/home-manager/ui/rofi/config.nix
@@ -0,0 +1,57 @@
+{ ... }:
+{
+  flake.modules.homeManager.ui = { ... }:
+  {
+    programs.rofi.theme = {
+      window = {
+        padding = mkLiteral "1em";
+        border = mkLiteral "1px";
+        border-radius = mkLiteral "10px";
+        width = mkLiteral "1024px";
+      };
+
+      mainbox = {
+        background-color = mkLiteral "transparent";
+        spacing = mkLiteral "8px";
+        children = mkLiteral "[inputbar,wrapper-mode-switcher,listview]";
+      };
+
+      "listview, inputbar, message" = {
+        padding = mkLiteral "0.5em";
+        border = mkLiteral "1px";
+        border-radius = mkLiteral "10px";
+        background-color = mkLiteral "transparent";
+
+        columns = mkLiteral "4";
+        lines = mkLiteral "4";
+      };
+
+      listview = {
+        border = mkLiteral "0px 5px 5px 5px";
+        border-radius = mkLiteral "0px 0px 10px 10px";
+      };
+
+      wrapper-mode-switcher = {
+        orientation = mkLiteral "horizontal";
+        expand = mkLiteral "false";
+        spacing = mkLiteral "0";
+      };
+
+      element = {
+        border = mkLiteral "0";
+        padding = mkLiteral "10";
+        orientation = mkLiteral "vertical";
+        background-color = mkLiteral "transparent";
+      };
+
+      element-icon = {
+        size = mkLiteral "5em";
+      };
+
+      "element-text, element-icon" ={
+        font = mkLiteral "inherit";
+        horizontal-align = mkLiteral "0.5";
+      };
+    };
+  };
+}
