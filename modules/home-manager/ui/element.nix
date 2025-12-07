888e0daa264781f572b76a8938934f5c2e19e7de
lamar daughma <lite5h4dow+dev@gmail.com>
2025-12-07 01:00:58 +0000

    adding flag wrapper

--- modules/home-manager/ui/element.nix
+++ modules/home-manager/ui/element.nix
@@ -1,5 +1,17 @@
 {
-  flake.modules.homeManager.ui = {
+  flake.modules.homeManager.ui = {pkgs,...}:
+  let
+    element' = pkgs.symlinkJoin{
+      name = "element-desktop";
+      paths = [ pkgs.element-desktop ];
+      nativeBuildInputs = [ pkgs.makeBinaryWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/element-desktop \
+        --add-flag "--password-store=gnome-libsecret"
+      '';
+    };
+  in
+  {
     programs.element-desktop = {
       enable = true;
     };
