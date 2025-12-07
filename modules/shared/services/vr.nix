e87f1a7e4f13913b96ac3233fcaa982b591cf63e
lamar daughma <lite5h4dow+dev@gmail.com>
2025-12-07 19:15:16 +0000

    wivrn as a service

--- modules/shared/services/vr.nix
+++ modules/shared/services/vr.nix
@@ -3,6 +3,7 @@
   flake.modules.nixos.vr = {
     services.wivrn = {
       enable = true;
+      autoStart = true;
       defaultRuntime = true;
       highPriority = true;
       openFirewall = true;
