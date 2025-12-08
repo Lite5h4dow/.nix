065478503a8b9a9412dc43ee90eb665108e331c5
lamar daughma <lite5h4dow+dev@gmail.com>
2025-12-08 11:53:28 +0000

    adding some stuff

--- modules/home-manager/ui/element.nix
+++ modules/home-manager/ui/element.nix
@@ -14,6 +14,7 @@
   {
     programs.element-desktop = {
       enable = true;
+      package = element';
     };
   };
 }
---
+++ modules/home-manager/ui/packages/atuin-desktop.nix
@@ -0,0 +1,8 @@
+{
+  flake.modules.homeManager.dev = {pkgs,...}:
+  {
+    home.packages = with pkgs; [
+      atuin-desktop
+    ];
+  };
+}
