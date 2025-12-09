a304f1eba49499bf6cd7d60d2ef6b1c330928e2b
lamar daughma <lite5h4dow+dev@gmail.com>
2025-12-09 13:52:56 +0000

    changing editor

--- modules/home-manager/base.nix
+++ modules/home-manager/base.nix
@@ -4,6 +4,9 @@
     home = {
       username = lib.mkDefault config.flake.meta.user.name;
       homeDirectory = lib.mkDefault "/home/${config.flake.meta.user.name}";
+      sessionVariables = {
+        EDITOR = "hx";
+      };
     };
     programs.home-manager.enable = true;
   };
