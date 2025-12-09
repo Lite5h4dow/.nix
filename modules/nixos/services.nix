04ccedd02a8362fee3f59ae7f17b864d5f7cfa2d
lamar daughma <lite5h4dow+dev@gmail.com>
2025-12-09 22:32:48 +0000

    adding ollama

--- modules/nixos/services.nix
+++ modules/nixos/services.nix
@@ -1,8 +1,18 @@
 {
-  flake.modules.nixos.base = {
-    services = {
-      gnome.gnome-keyring.enable = true;
-      sshd.enable = true;
+  flake.modules.nixos = {
+    base = {
+      services = {
+        gnome.gnome-keyring.enable = true;
+        sshd.enable = true;
+      };
+    };
+    extras = {
+      services = {
+        ollama = {
+          enable = true;
+          openFirewall = true;
+        };
+      };
     };
   };
 }
