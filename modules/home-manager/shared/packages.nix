72e280355e351f93c6d5d65814e0bbbcf578b14c
lamar daughma <lite5h4dow+dev@gmail.com>
2025-12-10 09:48:47 +0000

    adding gcc

--- modules/home-manager/shared/packages.nix
+++ modules/home-manager/shared/packages.nix
@@ -3,7 +3,10 @@
   flake.modules.homeManager = {
     dev = { pkgs, ... }:
     {
-      programs.go.enable = true;
+      programs = {
+        go.enable = true;
+        gcc.enable = true;
+      };
       home.packages = with pkgs; [
         pulumi-bin
       ];
