841d5e37d37a49209f9f705944fa55422b81e9ee
lamar daughma <lite5h4dow+dev@gmail.com>
2025-12-11 20:51:06 +0000

    update

--- modules/shared/services/dev.nix
+++ modules/shared/services/dev.nix
@@ -1,7 +1,20 @@
 {
   flake.modules.nixos = {
     dev = {
-      services.openbao.enable = true;
+      services = {
+        openbao = {
+          enable = true;
+          settings = {
+            ui = false;
+            cluster_addr = "http://127.0.0.1:8201";
+            api_addr = "https://bao.litelot.us";
+            listener.default = {
+              type = "unix";
+            };
+            storage.raft.path = "/var/lib/openbao";
+          };
+        };
+      };
     };
   };
 }
