31c003c8622a5d0ce7534fa8af06e42acaeda207
lamar daughma <lite5h4dow+dev@gmail.com>
2025-12-11 16:21:51 +0000

    updating

--- modules/shared/services/vr.nix
+++ modules/shared/services/vr.nix
@@ -10,19 +10,24 @@
         openFirewall = true;
         steam.importOXRRuntimes = true;
 
-        package = pkgs.wivrn.overrideAttrs (old: {
-          # version = "42d825883a409f4bd251e6072b549627aa4121cd";
-          # src = inputs.wivrn-custom;
-          src = lib.fetchFromGithub {
-            type = "github";
-            owner = "notpeelz";
-            repo = "WiVRn";
-            ref = "refs/heads/solarxr-patches";
-          };
-          cmakeFlags = old.cmakeFlags ++ [
-            (lib.cmakeBool "WIVRN_FEATURE_SOLARXR" true)
-          ];
-        });
+        # package = pkgs.wivrn.overrideAttrs (old: rec{
+        #   version = "42d825883a409f4bd251e6072b549627aa4121cd";
+        #   src = inputs.wivrn-custom;
+        #   cmakeFlags = old.cmakeFlags ++ [
+        #     # (lib.cmakeBool "WIVRN_FEATURE_SOLARXR" true)
+        #   ];
+
+        #   postUnpack = ''
+        #     ourMonadoRev="${old.monado.src.rev}"
+        #     theirMonadoRev=$(cat ${old.src.out}/monado-rev)
+        #     if [ ! "$theirMonadoRev" == "$ourMonadoRev" ]; then
+        #       echo "Our Monado source revision doesn't match CMakeLists.txt." >&2
+        #       echo "  theirs: $theirMonadoRev" >&2
+        #       echo "    ours: $ourMonadoRev" >&2
+        #       return 1
+        #     fi
+        #   '';
+        # });
       };
     };
 }
